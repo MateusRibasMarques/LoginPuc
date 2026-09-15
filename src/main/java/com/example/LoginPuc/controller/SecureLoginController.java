d855810c0f9a5ba380e44c7c84b4e39b647b231e
Mateus Marcal Ribas Marques <1583479@sga.pucminas.br>
2026-09-15 18:24:20 -0300

    feat: add dependencies

--- src/main/java/com/example/LoginPuc/controller/SecureLoginController.java
+++ src/main/java/com/example/LoginPuc/controller/SecureLoginController.java
@@ -2,8 +2,6 @@ package com.example.LoginPuc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 //@restController: API Rest
 @Controller
@@ -12,18 +10,10 @@ public class SecureLoginController {
     public String login() {
         return "login";
     }
-    
 
-      @PostMapping("/register")
-    public String handleRegister(
-            @RequestParam("nome") String nome,
-            @RequestParam("senha") String senha) {
-
-        // Aqui você pode adicionar lógica para salvar os dados do usuário, por exemplo:
-        // userService.saveUser(new User(nome, email, cpf, rg, endereco, instituicao, senha));
-
-        // Redirecionar ou exibir uma mensagem de sucesso
-        System.out.println("Registro: Redirecionado para a página de login.");
-        return "redirect:/login"; 
+    @GetMapping("/error")
+    public String error() {
+        return "error";
     }
+
 }
