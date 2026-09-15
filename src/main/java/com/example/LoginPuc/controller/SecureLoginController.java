4e7e7c7628e740907b87e4397c9ab4e5a824f0da
Mateus Marcal Ribas Marques <1583479@sga.pucminas.br>
2026-09-15 18:06:47 -0300

    feat: add SecureLogin

--- src/main/java/com/example/LoginPuc/controller/SecureLoginController.java
+++ src/main/java/com/example/LoginPuc/controller/SecureLoginController.java
@@ -2,6 +2,8 @@ package com.example.LoginPuc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //@restController: API Rest
 @Controller
@@ -12,5 +14,16 @@ public class SecureLoginController {
     }
     
 
-    
+      @PostMapping("/register")
+    public String handleRegister(
+            @RequestParam("nome") String nome,
+            @RequestParam("senha") String senha) {
+
+        // Aqui você pode adicionar lógica para salvar os dados do usuário, por exemplo:
+        // userService.saveUser(new User(nome, email, cpf, rg, endereco, instituicao, senha));
+
+        // Redirecionar ou exibir uma mensagem de sucesso
+        System.out.println("Registro: Redirecionado para a página de login.");
+        return "redirect:/login"; 
+    }
 }
